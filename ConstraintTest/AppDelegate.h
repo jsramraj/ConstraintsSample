7c3ddde17be29011f501aed9ad8938d319bf31fe
Ramaraj T <ramarajthanga.p@zanec.com>
2015-09-04 11:39:18 +0530

    Initial Commit

---
+++ ConstraintTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConstraintTest
+//
+//  Created by Ramaraj Thanga Pandi on 04/09/15.
+//  Copyright (c) 2015 AlcoChange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConstraintTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConstraintTest
+//
+//  Created by Ramaraj Thanga Pandi on 04/09/15.
+//  Copyright (c) 2015 AlcoChange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
